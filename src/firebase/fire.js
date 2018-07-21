331e926075fc50d0886632db32bc07943a013640
Andrew-Tran <andrew.tran@workday.com>
2018-07-21 13:35:26 -0700

    Moved API Key to config file

--- src/firebase/fire.js
+++ src/firebase/fire.js
@@ -1,7 +1,8 @@
 import firebase from 'firebase';
+import FIRE_CONFIG from './fire-config';
 
 var config = {
-  apiKey: "",
+  apiKey: FIRE_CONFIG.API_KEY,
   authDomain: "reunitefamilies-28bee.firebaseapp.com",
   databaseURL: "https://reunitefamilies-28bee.firebaseio.com",
   projectId: "reunitefamilies-28bee",
